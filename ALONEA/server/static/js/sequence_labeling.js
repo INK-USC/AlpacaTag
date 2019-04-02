fb86545ff14bfecf662e491998e96a686779ce74
danny911kr <danny911kr@naver.com>
2019-04-02 16:58:50 -0700

    tooltip shortkey

--- ALONEA/server/static/js/sequence_labeling.js
+++ ALONEA/server/static/js/sequence_labeling.js
@@ -104,45 +104,6 @@ Vue.component('annotator', {
       }
     },
 
-    validRecommendRange(startOffset, endOffset) {
-      if (startOffset === endOffset) {
-        return false;
-      }
-      if (startOffset > this.text.length || endOffset > this.text.length) {
-        return false;
-      }
-      if (startOffset < 0 || endOffset < 0) {
-        return false;
-      }
-      for (let i = 0; i < this.entityPositions.length; i++) {
-        const e = this.entityPositions[i];
-        if ((e.start_offset <= startOffset) && (startOffset < e.end_offset)) {
-          return false;
-        }
-        if ((e.start_offset < endOffset) && (endOffset < e.end_offset)) {
-          return false;
-        }
-        if ((startOffset < e.start_offset) && (e.start_offset < endOffset)) {
-          return false;
-        }
-        if ((startOffset < e.end_offset) && (e.end_offset < endOffset)) {
-          return false;
-        }
-      }
-      return true;
-    },
-
-    addLabelRecommend(labelId, startoff, endoff) {
-      if (this.validRecommendRange(startoff, endoff)) {
-        const label = {
-          start_offset: startoff,
-          end_offset: endoff,
-          label: labelId,
-        };
-        this.$emit('add-label', label);
-      }
-    },
-
     removeLabel(index) {
       this.$emit('remove-label', index);
     },
@@ -155,6 +116,11 @@ Vue.component('annotator', {
         end_offset: endOffset,
       };
       return label;
+    },
+
+    recommendShortkeyOffset(startOffset, endOffset){
+      this.startOffset = startOffset;
+      this.endOffset = endOffset;
     },
   },
 
@@ -195,12 +161,6 @@ Vue.component('annotator', {
         text_decoration: '',
       };
 
-      id2label[-2] = {
-        text_color: 'white',
-        background_color: 'black',
-        text_decoration: 'underline',
-      };
-
       for (let i = 0; i < this.labels.length; i++) {
         const label = this.labels[i];
         label.text_decoration = '';
@@ -211,37 +171,33 @@ Vue.component('annotator', {
   },
 });
 
-Vue.use(require('vue-shortkey'), {
-  prevent: ['input', 'textarea'],
-});
-
 Vue.component('recommender', {
-  template: '<div @click="setSelectedRange">\
-                 <template v-for="r in chunks">\
+  template: '<div><template v-for="r in chunks">\
+               <span @click="setSelectedRange(r.start_offset, r.end_offset, id2label[r.label].text_decoration)">\
                  <v-popover style="display: inline" offset="4" v-if="id2label[r.label].text_decoration">\
-                    <span class="text-sequence tooltip-target"\
-                         v-bind:class="{tag: id2label[r.label].text_color}"\
-                         v-bind:style="{textDecoration: id2label[r.label].text_decoration}"\
-                    >{{ text.slice(r.start_offset, r.end_offset) }}</span>\
-                    <template slot="popover">\
-                          <div class="card-header-title has-background-royalblue" style="padding:1.5rem;">\
-                            <div class="field is-grouped is-grouped-multiline">\
-                              <div class="control" v-for="label in labels">\
-                                <div class="tags has-addons">\
-                                  <a class="tag is-medium" v-bind:style="{ color: label.text_color, backgroundColor: label.background_color }"\
-                                      v-on:click="annotate(label.id, r.start_offset, r.end_offset)" v-shortkey.once=" replaceNull(label.shortcut) " @shortkey="annotate(label.id, r.start_offset, r.end_offset)">{{ label.text }}</a>\
-                                  <span class="tag is-medium"><kbd>{{ label.shortcut | simpleShortcut }}</kbd></span>\
+                          <span class="text-sequence tooltip-target"\
+                               v-bind:class="{tag: id2label[r.label].text_color}"\
+                               v-bind:style="{textDecoration: id2label[r.label].text_decoration}"\
+                          >{{ text.slice(r.start_offset, r.end_offset) }}</span>\
+                          <template slot="popover">\
+                                <div class="card-header-title has-background-royalblue" style="padding:1.5rem;">\
+                                  <div class="field is-grouped is-grouped-multiline">\
+                                    <div class="control" v-for="label in labels">\
+                                      <div class="tags has-addons">\
+                                        <a class="tag is-medium" v-bind:style="{ color: label.text_color, backgroundColor: label.background_color }"\
+                                            v-on:click="annotate(label.id, r.start_offset, r.end_offset)" >{{ label.text }}</a>\
+                                        <span class="tag is-medium"><kbd>{{ label.shortcut | simpleShortcut }}</kbd></span>\
+                                      </div>\
+                                    </div>\
+                                  </div>\
                                 </div>\
-                              </div>\
-                            </div>\
-                          </div>\
-                    </template>\
-                  </v-popover>\
-                  <span v-else class="text-sequence" \
-                  v-bind:class="{tag: id2label[r.label].text_color}"\
-                  v-bind:style="{textDecoration: id2label[r.label].text_decoration}">{{ text.slice(r.start_offset, r.end_offset) }}</span>\
-               </template>\
-               </div>',
+                          </template>\
+                        </v-popover>\
+                        <span v-else class="text-sequence" \
+                        v-bind:class="{tag: id2label[r.label].text_color}"\
+                        v-bind:style="{textDecoration: id2label[r.label].text_decoration}">{{ text.slice(r.start_offset, r.end_offset) }}</span>\
+                   </span></template></div>',
+
   props: {
     labels: Array, // [{id: Integer, color: String, text: String}]
     text: String,
@@ -255,27 +211,11 @@ Vue.component('recommender', {
   },
   mixins: [annotationMixin],
   methods: {
-    setSelectedRange(e) {
-      let start_rec;
-      let end_rec;
-      if (window.getSelection) {
-        const range = window.getSelection().getRangeAt(0);
-        const preSelectionRange = range.cloneRange();
-        preSelectionRange.selectNodeContents(this.$el);
-        preSelectionRange.setEnd(range.startContainer, range.startOffset);
-        start_rec = preSelectionRange.toString().length;
-        end_rec = start_rec + range.toString().length;
-      } else if (document.selection && document.selection.type !== 'Control') {
-        const selectedTextRange = document.selection.createRange();
-        const preSelectionTextRange = document.body.createTextRange();
-        preSelectionTextRange.moveToElementText(this.$el);
-        preSelectionTextRange.setEndPoint('EndToStart', selectedTextRange);
-        start_rec = preSelectionTextRange.text.length;
-        end_rec = start_rec + selectedTextRange.text.length;
+    setSelectedRange(startoff, endoff, label) {
+      if (label == 'underline') {
+        this.$parent.recommend_shortkey_offset(startoff, endoff);
+        console.log(startoff, endoff);
       }
-      this.startOffset = start_rec;
-      this.endOffset = end_rec;
-      console.log(start_rec, end_rec);
     },
 
     makeLabel(startOffset, endOffset) {
@@ -290,15 +230,9 @@ Vue.component('recommender', {
 
     annotate(labelId, startoff, endoff) {
       console.log(labelId, startoff, endoff);
-      this.$parent.recommend_annotate(labelId, startoff, endoff);
-    },
-
-    replaceNull(shortcut) {
-      if (shortcut === null) {
-        shortcut = '';
-      }
-      shortcut = shortcut.split(' ');
-      return shortcut;
+      this.$parent.recommend_shortkey_offset(startoff, endoff);
+      this.$parent.annotate(labelId);
+      this.$parent.resetRange();
     },
   },
 
@@ -351,8 +285,12 @@ const vm = new Vue({
       this.$refs.annotator.addLabel(labelId);
     },
 
-    recommend_annotate(labelId, startoff, endoff){
-      this.$refs.annotator.addLabelRecommend(labelId, startoff, endoff);
+    recommend_shortkey_offset(startoff, endoff){
+      this.$refs.annotator.recommendShortkeyOffset(startoff, endoff);
+    },
+
+    resetRange(){
+      this.$refs.annotator.resetRange();
     },
 
     addLabel(annotation) {
