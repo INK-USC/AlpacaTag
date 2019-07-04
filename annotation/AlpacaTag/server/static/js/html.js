ec46c4fe878c18e5c14494e369c00bb6f1c5661d
Dongho Lee <danny911kr@naver.com>
2019-07-04 15:23:34 -0700

    fix collapse

--- annotation/AlpacaTag/server/static/js/html.js
+++ annotation/AlpacaTag/server/static/js/html.js
@@ -1,26 +1,4 @@
-console.log('1')
-
-// document.getElementById('apitest').click(function (e) {
-//     console.log('111111')
-//
-//     document.getElementById('web').collapse('hide');
-//
-//     console.log('2')
-// });
-//
-
-
-document.getElementById('api-test-btn').onclick = function() {
-    alert( "Handler for .click() called." );
-}
-
-// $("#test_api_btn").click(function () {
-//
-//     alert( "Handler for .click() called." );
-//
-//     console.log('111111')
-//
-//     $('#web').collapse('hide');
-//
-//     console.log('2')
-// });
+var $myGroup = $('#tutorial');
+$myGroup.on('show.bs.collapse','.collapse', function() {
+    $myGroup.find('.collapse.show').collapse('hide');
+});
