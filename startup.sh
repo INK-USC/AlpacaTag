405dd6e2654c85eed5f20e2760a139f72a70e954
ddelange <14880945+ddelange@users.noreply.github.com>
2020-12-13 00:54:54 +0100

    Adopt doccano run.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,28 @@
-# create superuser from env
-echo 'import os; from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(os.environ["ADMIN_USERNAME"], os.environ["ADMIN_EMAIL"], os.environ["ADMIN_PASSWORD"])' | python manage.py shell
+#!/usr/bin/env bash
 
-# start model serving
+# https://github.com/doccano/doccano/blob/v1.0.5/tools/run.sh
+set -o errexit
+
+# start model serving in background
 alpaca-serving-start &
 
-# run annotation
-python manage.py runserver 0.0.0.0:8000
+# echo "Making staticfiles"
+# if [[ ! -d "app/staticfiles" ]]; then python app/manage.py collectstatic --noinput; fi
+
+echo "Initializing database"
+python manage.py wait_for_db
+python manage.py migrate
+python manage.py create_roles
+
+echo "Creating admin"
+if [[ -n "${ADMIN_USERNAME}" ]] && [[ -n "${ADMIN_PASSWORD}" ]] && [[ -n "${ADMIN_EMAIL}" ]]; then
+  python app/manage.py create_admin \
+    --username "${ADMIN_USERNAME}" \
+    --password "${ADMIN_PASSWORD}" \
+    --email "${ADMIN_EMAIL}" \
+    --noinput \
+  || true
+fi
+
+echo "Starting django"
+python manage.py runserver "0.0.0.0:${PORT:-8000}"
